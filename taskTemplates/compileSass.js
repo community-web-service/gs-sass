cd631031657bf0c99fd22565957703f370964352
Robert M. V. Gaines <rmvgaines@rgaineswebdevelopment.com>
2021-12-29 20:18:25 -0600

    Update Dependencies
    Updated dependencies and preformed minor refactoring.

--- taskTemplates/compileSass.js
+++ taskTemplates/compileSass.js
@@ -2,13 +2,16 @@
 
 module.exports = function compileSass(globSetGetter, options) {
 	var gulp = require("gulp");
-	var sass = require("gulp-sass");
+	var gulpSass = require("gulp-sass");
+	var nodeSass = require('node-sass');
 	var rename = require("gulp-rename");
 	var autoprefixer = require("gulp-autoprefixer");
 	var sourcemaps = require("gulp-sourcemaps");
 	var log = require("fancy-log");
 	var multiDest = require("gulp-multi-dest");
 	var concatCss = require('gulp-concat-css');
+
+	var sass = gulpSass(nodeSass);
 	
 	var globOptions = options.globOptions;
 	
@@ -19,8 +22,8 @@ module.exports = function compileSass(globSetGetter, options) {
 	var taskFunction = function () {
 		
 		var sassEntriesGlobSet = globSetGetter.getGlobSet(sassEntries);
-		var sassDirectoriesGlobSet = globSetGetter.getGlobSet(options.sassDirectories);
-		var destDirectoriesGlobSet = globSetGetter.getGlobSet(options.destDirectories);
+		var sassDirectoriesGlobSet = globSetGetter.getGlobSet(sassDirectories);
+		var destDirectoriesGlobSet = globSetGetter.getGlobSet(destDirectories);
 		
 		return gulp.src(sassEntriesGlobSet, globOptions)
 			.pipe(sourcemaps.init({
